7cb45c571c65142f133c9fe6c34dc2c8e0da57ef
Andrey Puhach <apugach@gomage.com>
2021-09-27 16:54:12 +0300

    + update 0.5.1

--- Tmodule/registration.php
+++ Tmodule/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_Tmodule',
+    'hellowMagento_Tmodule',
     __DIR__
 );
