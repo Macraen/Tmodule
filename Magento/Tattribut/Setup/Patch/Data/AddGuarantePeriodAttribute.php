464767198dad15afc06ac7f21ed40e179aa53650
Andrey Puhach <apugach@gomage.com>
2021-09-27 12:40:09 +0300

    Download module for git

---
+++ Magento/Tattribut/Setup/Patch/Data/AddGuarantePeriodAttribute.php
@@ -0,0 +1,90 @@
+<?php
+declare (strict_types=1);
+
+namespace Magento\Tattribut\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Class AddGuarantePeriodAttribute for Create Custom Product Attribute using Data Patch.
+ */
+class AddGuarantePeriodAttribute implements DataPatchInterface
+{
+
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute('catalog_product', 'guarantee_period', [
+            'type' => 'varchar',
+            'backend' => '',
+            'frontend' => '',
+            'label' => 'Guarantee terms',
+            'input' => 'text',
+            'class' => '',
+            'source' => '',
+            'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+            'visible' => true,
+            'required' => false,
+            'user_defined' => true,
+            'default' => '',
+            'searchable' => true,
+            'filterable' => true,
+            'comparable' => false,
+            'visible_on_front' => true,
+            'used_in_product_listing' => true,
+            'unique' => false,
+            'apply_to' => 'simple',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
