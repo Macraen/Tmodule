2882b3621b57b6db7a3e2466e63214e12c2171df
Andrey Puhach <apugach@gomage.com>
2021-09-28 15:53:12 +0300

    + update 0.6.5

---
+++ Plugin/Product.php
@@ -0,0 +1,8 @@
+<?php
+namespace Magento\Tmodule\Plugin;
+
+class Product{
+    public function afterGetName(\Magento\Catalog\Model\Product $subject, $result){
+        return "[BEST] ".$result;
+    }
+}
